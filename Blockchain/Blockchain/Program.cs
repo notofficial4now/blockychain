3ab3423cb10601e07a4a0e35540724ac355797bf
notofficial4now <110631861+notofficial4now@users.noreply.github.com>
2022-08-04 22:21:21 +0300

    Add files via upload

---
+++ Blockchain/Blockchain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Blockchain
+{
+    public class Program
+    {
+        ArrayList blockchain = new ArrayList();
+
+        public static void Main(string[] args)
+        {
+            string[] genesisTransactions = { "dani california" };
+            string[] transactions01 = { "donald trump" };
+
+            Block genesisBlock = new Block(0, genesisTransactions);
+            Block block01 = new Block(genesisBlock.getBlockHash(), transactions01);
+
+            Console.WriteLine(genesisBlock.getBlockHash());
+            Console.WriteLine(block01.getBlockHash());
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ Blockchain/Blockchain/blockchain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Security.Cryptography;
+
+namespace Blockchain
+{
+    class Block
+    {
+        private int PreviousHash;
+        private string[] transactions;
+        private int blockHash;
+        //Block genesisBlock = new Block(0, genesisTransactions);
+        public Block(int PreviousHash, string[] transactions)
+        {
+            this.PreviousHash = PreviousHash;
+            this.transactions = transactions;
+
+            Object[] contest = { PreviousHash, transactions.GetHashCode() };
+            int blockHash = contest.GetHashCode();
+        }
+        public int getPreviousHash()
+        {
+            return PreviousHash;
+        }
+        public string[] getTransactions()
+        {
+            return transactions;
+        }
+        public int getBlockHash()
+        {
+            return blockHash;
+        }
+
+    }
+
+}
